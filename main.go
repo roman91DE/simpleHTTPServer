3f369e601956ed7c7824b52f469d93d8b384ec96
rohoehn <rohoehn123@gmail.com>
2026-05-01 11:35:44 +0200

    finished

--- main.go
+++ main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	// "fmt"
+	"bufio"
+	"log"
 	"net/http"
-	// "log"
+	"os"
 )
 
-var mem map[string]bool
+var mem map[string][]string
 
-func initMEM() {
+func initMem() {
 	bufsize := 1024
-	mem = make(map[string]bool, bufsize)
+	mem = make(map[string][]string, bufsize)
+}
+
+func writeMem() {
+	file := "mem.txt"
+	w, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer w.Close()
+	writer := bufio.NewWriter(w)
+	for k := range mem {
+		for _, name := range mem[k] {
+			writer.WriteString(name)
+		}
+	}
+	writer.Flush()
 }
 
 func getNames(w http.ResponseWriter, r *http.Request) {
 	for k := range mem {
-		w.Write([]byte(k + "\n"))
-	}	
+		for _, name := range mem[k] {
+			w.Write([]byte(name))
+		}
+	}
 }
 func postName(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
@@ -24,12 +44,14 @@ func postName(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "name is required", http.StatusBadRequest)
 		return
 	}
-	mem[name] = true
+	clientIP := r.RemoteAddr
+	mem[clientIP] = []string{name + "\n"}
 	w.Write([]byte("ok"))
 }
 
 func main() {
-	initMEM()
+	initMem()
+	defer writeMem()
 	http.HandleFunc("GET /names", getNames)
 	http.HandleFunc("POST /names", postName)
 	http.ListenAndServe(":8080", nil)
